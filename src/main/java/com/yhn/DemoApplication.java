0420fdfff517c3540fa437e3b78dd1630fa74ff6
1197063733@qq.com <1197063733@qq.com>
2020-02-02 12:35:36 +0800

    11

--- src/main/java/com/yhn/DemoApplication.java
+++ src/main/java/com/yhn/DemoApplication.java
@@ -8,7 +8,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println(11);
+        System.out.println(111);
+
     }
 
 }
